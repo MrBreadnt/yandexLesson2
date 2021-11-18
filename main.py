ef5f65d4145f80c608830b6ffd6b4bb812892e6d
MrBreadnt <stena.oreh@mail.ru>
2021-11-18 18:11:28 +0700

    the commit number two

--- main.py
+++ main.py
@@ -5,26 +5,27 @@ from PyQt5 import uic
 from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from window import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('window.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.circles = []
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
-        x, y = (self.width()//2, self.height()//2)
+        x, y = (self.width() // 2, self.height() // 2)
         for i in self.circles:
-            qp.drawEllipse(QPoint(x, y), i, i)
+            qp.setBrush(i[1])
+            qp.drawEllipse(QPoint(x, y), i[0], i[0])
         qp.end()
 
     def run(self):
-        self.circles.append(randint(1, 300))
+        self.circles.append((randint(1, 300), QColor(randint(0, 256), randint(0, 256), randint(0, 256))))
         self.repaint()
 
 
---
+++ window.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtCore, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(390, 290, 31, 31))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "кнопка"))
